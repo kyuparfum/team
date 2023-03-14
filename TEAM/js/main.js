2b3c3f654077f4eea866305b624d8407fd3d6556
kyuparfum <123526355+kyuparfum@users.noreply.github.com>
2023-03-14 09:16:16 +0900

    Add files via upload

--- TEAM/js/main.js
+++ TEAM/js/main.js
@@ -1,15 +1,17 @@
 window.onload=function(){
     let open_btn=document.querySelector('.open_btn')
     let close_btn=document.querySelector('.close_btn')
+    let close_btn_img=document.querySelector('.close_btn img')
     let hide_list=document.querySelector('.hide_list')
-    console.log(open_btn,close_btn)
+    console.log(close_btn_img,close_btn)
 
     open_btn.addEventListener('click',function(){
         hide_list.style.display='block'
         open_btn.style.display='none'
         close_btn.style.display='flex'
-        close_btn.style.transform='rotate(180deg);'
+        close_btn_img.style.transform='rotate(180deg)'
     })
+    
     close_btn.addEventListener('click',function(){
         hide_list.style.display='none'
         open_btn.style.display='flex'
